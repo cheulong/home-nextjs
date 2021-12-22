a8c88ba5206be0c32ce428978b3b88cef4d6504c
Cheulong Sear <searcheulong@gmail.com>
2021-12-22 21:37:50 +0700

    finish

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/layout';
+
+const Footer = () => (
+  <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='#22ffe2'>
+    © 2021 Realtor, Inc.
+  </Box>
+);
+
+export default Footer;
